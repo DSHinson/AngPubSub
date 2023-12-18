d8dadfa5e3196f61ad030d9bd3de6d85c3189122
Daniel Hinson <DSHinson@outlook.com>
2023-12-18 23:38:00 +0000

    Fix: auth service unsubscribe on destroy

--- src/app/services/user/auth.service.ts
+++ src/app/services/user/auth.service.ts
@@ -1,23 +1,28 @@
-import { Injectable } from '@angular/core';
+import { Injectable, OnDestroy } from '@angular/core';
 import { BehaviorSubject, Observable } from 'rxjs';
 import {EventBusService}from '../events/eventbus.service'
+import { SubSink } from 'subsink';
 
 @Injectable({
   providedIn: 'root'
 })
-export class AuthService {
-
+export class AuthService implements OnDestroy {
+  private subs = new SubSink();
 
 
   private user:BehaviorSubject<any>= new BehaviorSubject({});
 
   constructor(private EventBusService: EventBusService) { 
-    this.EventBusService.on("login:attempt").subscribe((data: any)=>{
+    this.subs.sink=this.EventBusService.on("login:attempt").subscribe((data: any)=>{
       this.user.next(data);
     });
   }
+
   public currentUser():Observable<any>{
     return this.user.asObservable();
+  }
+  public ngOnDestroy(): void {
+    this.subs.unsubscribe()
   }
 
 }
